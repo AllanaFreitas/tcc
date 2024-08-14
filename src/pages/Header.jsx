3265d7d2c500f62cb778055be165098756e9281c
AllanaFreitas <fallana807@gmail.com>
2024-08-14 20:16:24 -0300

    14/08/24

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,29 +4,19 @@ import IconeJanela from '../public/IconeJanela.png'
 export default function Header() {
   return (
     <div>
-        <img src= {IconeJanela} alt="icone janela do" width={170} />
-        
-        <div>
-        </div>
-        
-        <nav className='text-space-blue font-kulim'>  
-        <ul>
-            <a href="">Inicio</a>
-        </ul>          
-        <ul>
-                <a href="">Funcionamento</a>
-         </ul>
-         <ul>
-                <a href="">Materiais</a>
-         </ul>
-        <ul>
-                <a href="">Referencias</a>
-        </ul>
-        <ul>
-                <a href="">Integrantes</a>
-        </ul>
+        <header className='flex'>
+                <img src={IconeJanela} className='h-16 w-' alt="icone janela logotipo" />
 
-        </nav>
+                <nav>
+                        <ul className='flex font-kulim '>
+                                <li><a href="">Início</a></li>
+                                <li><a href="">Funcionamento</a></li>
+                                <li><a href="">Materiais</a></li>
+                                <li><a href="">Referências</a></li>
+                                <li><a href="">Integrantes</a></li>
+                        </ul>
+                </nav>
+        </header>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
+import robohome from '../public/robohome.png'
 
 export default function Home() {
   return (
     <div>
-        <section className=''>
+        <section className='text-center'>
           <h1 className='font-kulim text-dark-blue text-7xl ,font-bold, text-center '>
-            Venha Conhecer a janela inteligente!
+            Venha Conhecer 
           </h1>
+          <h1 className='font-kulim text-dark-blue text-7xl ,font-bold, text-center '>a janela inteligente!</h1>
 
           <button><a href="" className=''>Saiba mais</a></button>
 
-          <img src="" alt="" />
+          <img src= {robohome} alt="" width={400} className=''/>
         </section>
         
 
 
-        <section className=''>
+        <section className='text-center'>
           <p className='font-kulim'>2</p>
 
-          <h1 className='font-kulim'>Qual o nosso diferencial?</h1>
+          <h1 className='font-kulim text-orange text-7xl'>Qual o nosso </h1>
+          <h1 className='font-kulim text-orange text-7xl'>diferencial?</h1>
 
-          <p className='font-kulim'>Nossa Smart Window (Janela Inteligente) oferece a segurança que a sua residência!</p>
+          <p className='font-kulim text-space-blue'>Nossa Smart Window (Janela Inteligente) oferece a segurança que a sua residência!</p>
 
           <p className='font-kulim'>Oferecemos segurança e tecnologia à sua casa, para proteger de fortes chuvas, umidade, fumaça e também substâncias tóxicas pelo ar, simplesmente usando o sistema Arduino, onde o projeto irá identificar umidade ao redor e fumaça para automaticamente fechar a janela.</p>
 
